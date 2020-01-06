3912571cb2e98997f26abb3b644d5b383c56e175
cleedus <cletus.mukolu@gmail.com>
2020-01-06 06:02:13 -0600

    fixing linting error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 app.get('/', (req, res) => {
     res.send("Welcome to my capstone app deployed with kubernetes. This is the green deployment version")
-});;
+});
 
 
 app.listen(80, () => {
